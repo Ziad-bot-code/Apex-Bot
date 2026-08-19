8bbc6da912ddc47a7d74ba877846bb5c374e918e
Ziad-bot-code <ziadg1506@gmail.com>
2026-08-19 23:15:52 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "EuropeMC Giveaways Hub", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        state: "By _ziad1_",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
