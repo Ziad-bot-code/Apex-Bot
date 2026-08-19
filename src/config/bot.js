c83e0a57f7916f2c2f1a17e649af1002efb52862
Ziad-bot-code <ziadg1506@gmail.com>
2026-08-20 00:13:06 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -277,7 +277,7 @@ export const botConfig = {
     maximumDuration: 2592000000,
 
     // Role IDs allowed to host giveaways.
-    allowedRoles: [],
+    allowedRoles: [1539256316827013150],
 
     // Role IDs that bypass giveaway restrictions.
     bypassRoles: [],
