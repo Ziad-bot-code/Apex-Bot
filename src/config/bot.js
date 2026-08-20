7deec60c222d69f8565ef67c116476deb297f7ac
Ziad-bot-code <ziadg1506@gmail.com>
2026-08-20 15:10:52 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Echo Bot", // required by Discord API, not shown in the client
+        name: "Apex Bot", // required by Discord API, not shown in the client
         state: "Bot Made By _ziad1_ ",     // this is what people actually see
         type: 4,               // Custom
       },
