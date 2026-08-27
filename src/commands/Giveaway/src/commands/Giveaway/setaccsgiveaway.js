b0ded3a29b1315bab8be89fddf7271fbe51513ca
Ziad-bot-code <ziadg1506@gmail.com>
2026-08-27 21:14:25 +0300

    Create setaccsgiveaway.js

---
+++ src/commands/Giveaway/src/commands/Giveaway/setaccsgiveaway.js
@@ -0,0 +1,48 @@
+import { SlashCommandBuilder, PermissionFlagsBits, MessageFlags } from 'discord.js';
+import { successEmbed } from '../../utils/embeds.js';
+import { TitanBotError, ErrorTypes } from '../../utils/errorHandler.js';
+import { InteractionHelper } from '../../utils/interactionHelper.js';
+import { updateGuildConfig } from '../../services/config/guildConfig.js';
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("setaccsgiveaway")
+        .setDescription("Set the role allowed to host giveaways.")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .addRoleOption((option) =>
+            option
+                .setName("role")
+                .setDescription("Select the role or paste the role ID allowed to run giveaways.")
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        await InteractionHelper.safeDefer(interaction, { flags: MessageFlags.Ephemeral });
+
+        if (!interaction.inGuild()) {
+            throw new TitanBotError(
+                'Command used outside guild',
+                ErrorTypes.VALIDATION,
+                'This command can only be used in a server.',
+                { userId: interaction.user.id }
+            );
+        }
+
+        const role = interaction.options.getRole("role");
+
+        // Save the role ID to database config
+        await updateGuildConfig(interaction.client, interaction.guildId, {
+            giveawayAccessRoleId: role.id
+        });
+
+        await InteractionHelper.safeReply(interaction, {
+            embeds: [
+                successEmbed(
+                    `Giveaway Access Updated! 🎉`,
+                    `Members with the ${role} role can now create giveaways using \`/gcreate\`.`
+                ),
+            ],
+            flags: MessageFlags.Ephemeral,
+        });
+    },
+};
